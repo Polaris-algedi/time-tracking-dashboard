9de53063c55cdbd59cb588f47ed27a86cb1f6663
Polaris-algedi <karchalmohammed@gmail.com>
2025-08-22 17:19:14 +0100

    Adds async data fetch and UI update for activity times
    Implements asynchronous retrieval of activity data from a JSON file and updates the UI to display daily, weekly, or monthly activity timeframes. Improves user experience by dynamically populating activity statistics on page load.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,78 @@
 import "./style.css";
+
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const data = await getData("data.json");
+  console.log(data);
+  const activityTimeList = document.querySelectorAll(".activity-time");
+  console.log(activityTimeList);
+
+  switch ("weekly") {
+    case "daily":
+      const dailyData = data.map((item) => ({
+        current: item.timeframes.daily.current,
+        previous: item.timeframes.daily.previous,
+      }));
+      // Update the UI with dailyData
+      dailyData.forEach((item, index) => {
+        const activityTime = activityTimeList[index];
+        if (activityTime) {
+          activityTime.firstElementChild.textContent = item.current + "hrs";
+          activityTime.lastElementChild.textContent =
+            "Last Day - " + item.previous + "hrs";
+        }
+      });
+      break;
+    case "weekly":
+      const weeklyData = data.map((item) => {
+        return {
+          current: item.timeframes.weekly.current,
+          previous: item.timeframes.weekly.previous,
+        };
+      });
+      // Update the UI with weeklyData
+      weeklyData.forEach((item, index) => {
+        const activityTime = activityTimeList[index];
+        if (activityTime) {
+          activityTime.firstElementChild.textContent = item.current + "hrs";
+          activityTime.lastElementChild.textContent =
+            "Last Week - " + item.previous + "hrs";
+        }
+      });
+      break;
+    case "monthly":
+      const monthlyData = [];
+      data.forEach((element) => {
+        monthlyData.push({
+          current: element.timeframes.monthly.current,
+          previous: element.timeframes.monthly.previous,
+        });
+      });
+      // Update the UI with monthlyData
+      monthlyData.forEach((item, index) => {
+        const activityTime = activityTimeList[index];
+        if (activityTime) {
+          activityTime.firstElementChild.textContent = item.current + "hrs";
+          activityTime.lastElementChild.textContent =
+            "Last Month - " + item.previous + "hrs";
+        }
+      });
+      break;
+    default:
+      console.log("Unknown timeframe");
+      break;
+  }
+});
